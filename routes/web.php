770921f3926d23092d5bbff37458428c3e3663c2
Ismael <kismaelkemal@gmail.com>
2023-10-22 12:47:31 +0300

    refactor: Update web route-controller setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuctionController;
+use App\Http\Controllers\BidController;
+use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SellerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,24 +25,24 @@ Route::get('/', function () {
 
 // Route::get('/login', 'Auth\LoginController@showLoginForm');
 // Route::get('/signup', 'Auth\RegisterController@showRegistrationForm');
-Route::get('/auctions', 'AuctionController@index');
-Route::get('/auctions/{auctionId}', 'AuctionController@show');
-Route::get('/auctions/{auctionId}/bid', 'BidController@bid');
-Route::get('/auctions/{auctionId}/history', 'AuctionController@history');
-Route::get('/profile', 'ProfileController@index');
+Route::get('/auctions', [AuctionController::class, 'index']);
+Route::get('/auctions/{auctionId}', [AuctionController::class, 'show']);
+Route::get('/auctions/{auctionId}/bid', [BidController::class, 'bid']);
+Route::get('/auctions/{auctionId}/history', [AuctionController::class, 'history']);
+Route::get('/profile', [ProfileController::class, 'index']);
 
 // Buyer Routes
 Route::group(['prefix' => 'buyer'], function () {
-    Route::get('/dashboard', 'BuyerController@dashboard');
-    Route::get('/auctions', 'BuyerController@auctions');
-    Route::get('/won-auctions', 'BuyerController@wonAuctions');
+    Route::get('/dashboard', [BuyerController::class, 'dashboard']);
+    Route::get('/auctions', [BuyerController::class, 'auctions']);
+    Route::get('/won-auctions', [BuyerController::class, 'wonAuctions']);
 });
 
 // Seller Routes
 Route::group(['prefix' => 'seller'], function () {
-    Route::get('/dashboard', 'SellerController@dashboard');
-    Route::get('/create-auction', 'SellerController@createAuction');
-    Route::get('/manage-auctions', 'SellerController@manageAuctions');
+    Route::get('/dashboard', [SellerController::class, 'dashboard']);
+    Route::get('/create-auction', [SellerController::class, 'createAuction']);
+    Route::get('/manage-auctions', [SellerController::class, 'manageAuctions']);
 });
 
 Auth::routes();
